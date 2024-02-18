5ba9a3c52a2ad5cd91c3bd98aef97e7287829582
hmzshin <hamzasahin@outlook.com>
2024-02-18 12:52:13 +0300

    create BurgerDao to define database operations

---
+++ src/main/java/com/workintech/s18d1/dao/BurgerDao.java
@@ -0,0 +1,25 @@
+package com.workintech.s18d1.dao;
+
+import java.util.List;
+
+import com.workintech.s18d1.entity.BreadType;
+import com.workintech.s18d1.entity.Burger;
+
+public interface BurgerDao {
+    Burger save(Burger burger);
+
+    List<Burger> findAll();
+
+    Burger findById(Long id);
+
+    Burger update(Burger burger);
+
+    Burger remove(Long id);
+
+    List<Burger> findByPrice(Double price);
+
+    List<Burger> findByBreadType(BreadType breadType);
+
+    List<Burger> findByContent(String content);
+
+}
